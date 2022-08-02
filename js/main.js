0f47f4fbadf98b2185ffb4d70fd40cd3cf8cf211
Evan Shaw <e85.shaw@gmail.com>
2022-08-02 13:42:51 -0700

    CSS style updates and JS update: included additional selectors that pull and display from the fetch.

--- js/main.js
+++ js/main.js
@@ -18,9 +18,15 @@ async function apiRequest() {
 const trickName = document.querySelector('#trickNameDisplay')
 const trickImage = document.querySelector('#trickImageDisplay')
 const skillLevel = document.querySelector('#skillLevelDisplay')
+const inventor = document.querySelector('#inventorDisplay')
+const yearCreated = document.querySelector('#yearCreated')
+const description = document.querySelector('#descriptionDisplay')
 
 function changeContent(data) {
     trickName.innerText = data.name
     skillLevel.innerText = data.skillLevel
-    trickImage.src = ""
+    inventor.textContent = data.inventor
+    yearCreated.textContent = data.yearCreated
+    description.textContent = data.description
+    // trickImage.src = ""
 }
